41a0a0cf16de816dad29ef6d7d5912fc51406101
pedro-novo <pedro.novo.93@gmail.com>
2023-12-14 07:53:35 +0000

    inital setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: Props) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
