2ba4097a10f39213faaff3d220313d22ed847f2e
pedro-novo <pedro.novo.93@gmail.com>
2023-12-15 08:54:28 +0000

    [SETUP] add all server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,5 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signIn();
-}
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { createPost } from "./create-post";
+export { createComment } from "./create-comment";
+export { createTopic } from "./create-topic";
