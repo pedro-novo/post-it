0efb7be7ba308bb40418d915136d90d3c4335d96
pedro-novo <pedro.novo.93@gmail.com>
2023-12-15 15:45:02 +0000

    add create topic server action with validation and error handling

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,77 @@
 "use server";
 
-export async function createTopic() {
-  // TODO: revalidate homepage
+import type { Topic } from "@prisma/client";
+import { auth } from "@/auth";
+import { z } from "zod";
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+import paths from "@/paths";
+import { revalidatePath } from "next/cache";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, { message: "Must be lowercase letters or dashes without spaces" }),
+  description: z.string().min(10),
+});
+
+type CreateTopicFormState = {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+};
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  const session = await auth();
+
+  if (!session || !session?.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in!"],
+      },
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    }
+
+    return {
+      errors: {
+        _form: ["Something went wrong."],
+      },
+    };
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicPage(topic.slug));
 }
