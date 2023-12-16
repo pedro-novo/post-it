61882ba1a7017191619d8a245c13b65464250e10
pedro-novo <pedro.novo.93@gmail.com>
2023-12-16 09:55:08 +0000

    add topic display page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,23 @@
-export default function TopicDisplay() {
-  return <div>Topic Display</div>;
+import PostCreateForm from "@/components/posts/post-create-form";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicDisplay({ params }: Props) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
+    </div>
+  );
 }
