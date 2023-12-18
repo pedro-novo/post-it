1d4ba6cbfc70c432207a8574e75ec751c244942d
pedro-novo <pedro.novo.93@gmail.com>
2023-12-18 09:30:01 +0000

    apply different approach by caching query  response

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,17 +1,17 @@
 import CommentShow from "@/components/comments/comment-show";
-import { CommentWithAuthor } from "@/db/queries/comments";
+import { db } from "@/db";
+import { CommentWithAuthor, fetchCommentsByPostId } from "@/db/queries/comments";
 
 interface CommentListProps {
-  fetchData: () => Promise<CommentWithAuthor[]>;
+  postId: string;
 }
 
-// TODO: Get a list of comments from somewhere
-export default async function CommentList({ fetchData }: CommentListProps) {
-  const comments = await fetchData();
+export default async function CommentList({ postId }: CommentListProps) {
+  const comments = await fetchCommentsByPostId(postId);
 
   const topLevelComments = comments.filter((comment) => comment.parentId === null);
   const renderedComments = topLevelComments.map((comment) => {
-    return <CommentShow key={comment.id} commentId={comment.id} comments={comments} />;
+    return <CommentShow key={comment.id} commentId={comment.id} postId={postId} />;
   });
 
   return (
