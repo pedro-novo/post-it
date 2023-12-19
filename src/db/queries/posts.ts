1b431a197773203fd4fe488842bc51c5237cdcf6
pedro-novo <pedro.novo.93@gmail.com>
2023-12-19 08:02:08 +0000

    add new top post query and render it in home page

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -19,3 +19,21 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostItemType[]> {
     },
   });
 }
+
+export function fetchTopPosts(): Promise<PostItemType[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: "desc",
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
+  });
+}
